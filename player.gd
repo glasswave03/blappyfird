885573f14eb7f7ab15e81d27fcea1d6daee5d8fa
glasswave03 <kissbandoo@gmail.com>
2026-01-28 16:50:37 +0100

    player controls, tiles

--- player.gd
+++ player.gd
@@ -1,1 +1,27 @@
 extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -250.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += (get_gravity()/2) * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("dive"):
+		velocity.y = SPEED
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
