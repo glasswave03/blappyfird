b2f9d54846a9a00135812bff4e687d72899ef779
Kiss András László <h264684@stud.u-szeged.hu>
2026-03-14 17:39:27 +0100

    coin +5 animation added

--- killzone.gd
+++ killzone.gd
@@ -4,17 +4,16 @@ var fail_scene = preload("res://fail_menu.tscn").instantiate()
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
+	# TODO: switch player to dead player sprite, make it fall on the ground 
+	#var pos = get_node("World/Game/Player").position
+	#var dead: Sprite2D
+	#dead.position = pos
+	body.queue_free()
 	Global.game_manager.clear_gui()
 	timer.start(1)
 	timer.ignore_time_scale = true
 	Engine.time_scale = 0.5
-	body.velocity = Vector2(0,0)
-	# TODO: switch player to dead player sprite, make it fall on the ground 
-	# TODO: change GUI scene to fail screen with highscore and score of the just failed run
 	
-	#var pos = get_node("World/Game/Player").position
-	#var dead: Sprite2D
-	#dead.position = pos
 	if (timer.is_stopped()): timer.timeout.emit()
 
 func _on_timer_timeout():
